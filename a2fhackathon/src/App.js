3421b962fd81c235c2ee6fe6cdf288c0f3344dbf
Edward Nawrocki <eddnawrocki@gmail.com>
2023-04-15 14:05:42 -0700

    Chat System With Socket.io

--- a2fhackathon/src/App.js
+++ a2fhackathon/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import Home from './routes/home';
 import { Routes, Route } from "react-router-dom";
 import socketIO from 'socket.io-client';
+import ChatPage from './routes/ChatPage';
 const socket = socketIO.connect('http://localhost:4000');
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={ <Home/> } />
+        <Route path="/" element={ <Home socket={socket}/> } />
+        <Route path="/chat" element={<ChatPage socket={socket} />}/>
       </Routes>
     </div>
   );
--- a2fhackathon/src/routes/home.js
+++ a2fhackathon/src/routes/home.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-export default function Home() {
-    return(
-        <React.Fragment>
-            <h1>Home Page</h1>
-        </React.Fragment>
-    );
-}
+const Home = ({socket}) => {
+  const navigate = useNavigate();
+  const [userName, setUserName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    localStorage.setItem('userName', userName);
+    socket.emit('newUser', {userName, socketID: socket.id});
+    navigate('/chat');
+  };
+  return (
+    <form className="home__container" onSubmit={handleSubmit}>
+      <h2 className="home__header">Sign in to Open Chat</h2>
+      <label htmlFor="username">Username</label>
+      <input
+        type="text"
+        minLength={6}
+        name="username"
+        id="username"
+        className="username__input"
+        value={userName}
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <button className="home__cta">SIGN IN</button>
+    </form>
+  );
+};
+
+export default Home;
